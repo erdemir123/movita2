5f018359fac8f44bed4d4874057942086d53526e
sadik <sadikerdemir123@icloud.com>
2023-12-28 17:30:58 +0300

    npm sildim

---
+++ .next/server/_rsc_messages_tr_json.js
@@ -0,0 +1,25 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+exports.id = "_rsc_messages_tr_json";
+exports.ids = ["_rsc_messages_tr_json"];
+exports.modules = {
+
+/***/ "(rsc)/./messages/tr.json":
+/*!**************************!*\
+  !*** ./messages/tr.json ***!
+  \**************************/
+/***/ ((module) => {
+
+module.exports = JSON.parse('{"Index":{"title":"merhaba"},"header":{"camera":"Kamera sistemleri","car":"Araç Takip Sistemleri","security":"Güvenlik Sistemleri","login":"Giriş"},"home":{"content":" movita, mobil araçlar için yerli mühendisler tarafından geliştirilen araç takip ve uzaktan izlemeli kamera kayıt özelliği bulunan bir çok yeni çözüm sunan sistemdir.","why":"Neden Movita ?","solutions":"Çözümlerimiz","code":"Satır Kod","customer":"Memnun Müşteri","solution":"Çözümler","year":"YIllık Deneyim"},"nav":{"about":"Hakkımızda","document":"Belgelerimiz","dealership":"Bayilik Başvurusu","demo":"Demo Bşavurusu","cookie_politic":"Çerez Politikası","kvkk":"Kvkk","artificial_intelligence":"Yapay Zeka","all":"Tümü","android":"Android","ios":"Ios","windows":"Windows","service_app_use":"movita Servis Kullanım Kılavuzu","movita_app_use":"movita Uygulama Kullanım Kılavuzu"},"navData":{"home":"Anasayfa","route_optimization":"Rota Optimizasyonu","communication":"İletişim","instutional":"Kurumsal","solutions":"Çözümlerimiz","apps":"Uygulamalar","guide":"Kılavuz","product":"Ürünlerimiz"},"about":{"about":"Hakkımızda","about_content":"Firmamız kurulduğu günden itibaren yenilikçi uygulamalarıyla ülkemizi bilgi teknolojileri alanında dünya ile yarışır kılmak, ülkemizin sektördeki dışa bağımlılığını azaltmak hedefiyle hizmet vermektedir. Farklı sektörlerdeki müşterileri için bugüne kadar geliştirilmiş olan tüm projelerde yenilik, yüksek teknoloji, kalite ve müşteri memnuniyeti konularında azami derecede hassasiyet gösterilmektedir. Karar destek sistemleri ve kurumsal kaynak yönetim sistemleri i le müşterilerine kendi varlıklarını etkili ve verimli bir şekilde yönetmesi için özel kurumsal bilişim çözümleri sunanFirmamız sektördeki yerini her geçen gün sağlamlaştırmaktadır. Öncelikli olarak insan hayatına, ülkesine ve dünyaya katma değerinin yüksek olduğuna inandığı Araştırma Geliştirme faaliyetlerini aralıksız devam ettiren Firmamız geleceğin bilgi teknolojilerine yön vermeyi ve Türkiyeye bu alanda saygınlık kazandırmayı hedeflemektedir.","mission":"Misyon","mission_content":"Firmamız; kendisinden çözüm bekleyen müşterilerini yüzde yüz memnun eden çözümler sunmayı, sunduğu çözümlerde son teknolojiler kullanmayı ve her zaman inovatif yaklaşımlar sergilemeyi, oluşturduğu ürünler ile dünyaya ve insana değer katmayı kendisine misyon edinmiştir.","vision":"Vizyon","vision_content":"Firmamız olarak vizyonumuz; müşteri odaklı bir yaklaşım ile faaliyet gösteren, müşterilerine her zaman yüksek kalite çözümler sunan, yurtiçinde ve yurtdışında bilişim sektöründe lider bir kuruluş haline gelmektir.","team":"Takım","team_content":"Ekibimizi oluştururken siz müşterilerimizin her türlü istek ve beklentisini tam olarak karşılamayı amaçlayan Firmamızın prensiplerine uygun bir yol izlemeye çalıştık. Bu sebeple her bir çalışanımız sizi memnun etmeyi ve işini en iyi şekilde yerine getirmeyi amaçlayan kişilerdir. Proje yöneticileri, yazılım mühendisleri, ar-ge mühendisleri, iş analistleri, grafik tasarımcılar, sistem ve veri tabanı yöneticileri barındıran takımımız genç ve dinamik ancak alanında tecrübeli bireylerden oluşmaktadır."},"cookies":{"header":" Çerez   Politikası"}}');
+
+/***/ })
+
+};
+;
---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"2QzdktqWalLDsS8KWqxQe4RNPGFQILDf60WhguI8xc0=\"\n}"
